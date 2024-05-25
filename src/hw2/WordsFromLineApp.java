b125e2eec34b207b38b3bc675c53a843fd2addbb
BiOksana <biokissme@gmail.com>
2024-05-25 19:50:13 +0200

    Initial commit.

---
+++ src/hw2/WordsFromLineApp.java
@@ -0,0 +1,8 @@
+package hw2;
+
+public class WordsFromLineApp {
+    public static void main(String[] args) {
+        String[] givenStringsArray = {"anger", "awe", "joy", "love", "grief"};
+        String str = "yliausoenvjw";
+    }
+}
